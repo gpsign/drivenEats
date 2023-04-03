61b31b56a9d1ab7a84f246f1160b4d2013072783
gpsign <gpsignorini@gmail.com>
2023-04-02 23:46:49 -0300

    Finalizado c:

--- JS/script.js
+++ JS/script.js
@@ -4,48 +4,35 @@ const header = document.querySelector(".header");
 const concluir = document.querySelector(".concluir");
 const finalizarSobrepor = document.querySelector(".finalizarSobrepor");
 const finalizar = document.querySelector(".finalizarSobrepor .finalizar");
-const usuarioSobrepor = document.querySelector(".usuarioSobrepor");
-const infoUsuario = document.querySelector(".finalizarSobrepor .infoUsuario");
-const nomeUsuario = document.querySelector(".infoUsuario .nomeUsuario");
-const enderecoUsuario = document.querySelector(".infoUsuario .enderecoUsuario");
-const inputNome = document.querySelector(".inputNome input");
-const inputEndereco = document.querySelector(".inputEndereco input")
-let total = '';
+const total = document.querySelector(".informacoesPedido .total").children[1];
 
 function selecionar(comida) {
   /*Verifica em qual lista foi selecionado*/
   let lista = "." + comida.parentElement.classList.item(1);
   let anterior = document.querySelector(lista + " .selecionado");
-
   carrinho++;
+  comida.classList.toggle("selecionado");
+  if(document.querySelector(lista + " .selecionado") == null)carrinho--;
 
   if (anterior != null) {
-    if (anterior != comida) anterior.classList.remove("selecionado");
-
-    if (anterior === comida) carrinho--;
-    if (
-      anterior.parentElement.classList.item(1) ==
-      comida.parentElement.classList.item(1)
-    ) {
-      carrinho--;
-    }
+    anterior.classList.remove("selecionado");
+    carrinho--;
   }
 
-  comida.classList.toggle("selecionado");
+  console.log(carrinho);
+  
   liberarBotao();
 }
 
 function liberarBotao() {
   let botao = document.querySelector(".concluir button");
-  if (carrinho != 3) botao.disabled = true;
-  if (carrinho === 1)
-    botao.innerHTML = "Selecione mais 2 itens<br />para fechar o pedido";
-  else if (carrinho === 2)
-    botao.innerHTML = "Selecione mais 1 item<br />para fechar o pedido";
-  else if (carrinho === 3) {
+  if (carrinho != 3) {
+    botao.innerHTML = "Selecione os 3 itens <br />para fechar o pedido";
+    botao.disabled = true;
+  } else {
     botao.innerHTML = "Fechar o pedido";
     botao.disabled = false;
-  } else botao.innerHTML = "Selecione os 3 itens<br />para fechar o pedido";
+  }
 }
 
 function calcular(prato, bebida, sobremesa) {
@@ -56,12 +43,22 @@ function calcular(prato, bebida, sobremesa) {
   return total.toFixed(2);
 }
 
+function fundoBlur() {
+  if (finalizarSobrepor.classList.contains("esconder")) {
+    conteudo.classList.add("blur");
+    header.classList.add("blur");
+    concluir.classList.add("blur");
+    finalizarSobrepor.classList.remove("esconder");
+  } else {
+    conteudo.classList.remove("blur");
+    header.classList.remove("blur");
+    concluir.classList.remove("blur");
+    finalizarSobrepor.classList.add("esconder");
+  }
+}
+
 function dialogPedido() {
-  /*Blur da tela e mostrar a caixa*/
-  conteudo.classList.add("blur");
-  header.classList.add("blur");
-  concluir.classList.add("blur");
-  finalizarSobrepor.classList.remove("esconder");
+  fundoBlur();
 
   /*Aplica nome e preco do prato na lista de compras*/
   let pratoNome = document.querySelector(".prato .selecionado .nome").innerHTML;
@@ -69,11 +66,9 @@ function dialogPedido() {
     ".prato .selecionado .preco"
   ).innerHTML;
   let info = document.querySelector(".pedido .prato");
-  let infoNome = info.children[0];
-  let infoPreco = info.children[1];
 
-  infoNome.innerHTML = pratoNome;
-  infoPreco.innerHTML = pratoPreco;
+  info.children[0].innerHTML = pratoNome;
+  info.children[1].innerHTML = pratoPreco;
 
   /*Aplica nome e preco da bebida na lista de compras*/
   let bebidaNome = document.querySelector(
@@ -84,11 +79,8 @@ function dialogPedido() {
   ).innerHTML;
 
   info = document.querySelector(".pedido .bebida");
-  infoNome = info.children[0];
-  infoPreco = info.children[1];
-
-  infoNome.innerHTML = bebidaNome;
-  infoPreco.innerHTML = bebidaPreco;
+  info.children[0].innerHTML = bebidaNome;
+  info.children[1].innerHTML = bebidaPreco;
 
   /*Aplica nome e preco da sobremesa na lista de compras*/
   let sobremesaNome = document.querySelector(
@@ -99,44 +91,38 @@ function dialogPedido() {
   ).innerHTML;
 
   info = document.querySelector(".pedido .sobremesa");
-  infoNome = info.children[0];
-  infoPreco = info.children[1];
+  info.children[0].innerHTML = sobremesaNome;
+  info.children[1].innerHTML = sobremesaPreco;
 
-  infoNome.innerHTML = sobremesaNome;
-  infoPreco.innerHTML = sobremesaPreco;
-
-  total = document.querySelector(".informacoesPedido .total").children[1];
   total.innerHTML =
     "R$ " + calcular(pratoPreco, bebidaPreco, sobremesaPreco).replace(".", ",");
 }
 
-function dialogUsuario(){
-  finalizarSobrepor.classList.add("esconder");
-  usuarioSobrepor.classList.remove("esconder");
-}
+function enviarPedido() {
+  let prato =
+    "\n- Prato: " +
+    document.querySelector(".prato .selecionado .nome").innerHTML;
+  let bebida =
+    "\n- Bebida: " +
+    document.querySelector(".bebida .selecionado .nome").innerHTML;
+  let sobremesa =
+    "\n- Sobremesa: " +
+    document.querySelector(".sobremesa .selecionado .nome").innerHTML;
 
-function cancelarPedido() {
-  conteudo.classList.remove("blur");
-  header.classList.remove("blur");
-  concluir.classList.remove("blur");
-  finalizarSobrepor.classList.add("esconder");
-}
+  let mensagem =
+    "Olá, gostaria de fazer o pedido:" +
+    prato +
+    bebida +
+    sobremesa +
+    "\nTotal: " +
+    total.innerHTML;
+  mensagem +=
+    "\n\nNome: " + prompt("Nome:") + "\nEndereco: " + prompt("Endereço");
 
-function cancelarDados(){
-  finalizarSobrepor.classList.remove("esconder");
-  usuarioSobrepor.classList.add("esconder");
-  inputEndereco.value = inputNome.value = '';
-}
-
-function enviarPedido(){
-  let prato = "\n- Prato: " + document.querySelector(".prato .selecionado .nome").innerHTML;
-  let bebida = "\n- Bebida: " + document.querySelector(".bebida .selecionado .nome").innerHTML;
-  let sobremesa = "\n- Sobremesa: " + document.querySelector(".sobremesa .selecionado .nome").innerHTML;
-
-  let mensagem = "Olá, gostaria de fazer o pedido:" + prato + bebida + sobremesa + "\nTotal: " + total.innerHTML;
-  mensagem += "\nNome: " + inputNome.value + "\nEndereco: " + inputEndereco.value;
-
-  alert("Voce sera redirecionado ao whatsapp");
-  window.open("https://wa.me/5545998491326?text=" + encodeURIComponent(mensagem), "_blank").focus();
-  inputEndereco.value = inputNome.value = '';
+  window
+    .open(
+      "https://wa.me/5545998491326?text=" + encodeURIComponent(mensagem),
+      "_blank"
+    )
+    .focus();
 }
